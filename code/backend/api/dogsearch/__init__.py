13e15882ae433370a58169b08d438063c0c50c53
Vadim Alimguzhin <vadim.alimguzhin@gmail.com>
2021-10-17 22:03:21 +0200

    Implement /attributes API endpoint.

--- code/backend/api/dogsearch/__init__.py
+++ code/backend/api/dogsearch/__init__.py
@@ -1,8 +1,11 @@
-from flask import Flask, g
+from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
+
+from dogsearch.attribute import AttributeList
 
 app = Flask(__name__)
 CORS(app)
 
 api = Api(app)
+api.add_resource(AttributeList, "/attributes")
---
+++ code/backend/api/dogsearch/attribute.py
@@ -0,0 +1,12 @@
+from flask_restful import Resource, abort
+from dogsearch.db import get_db
+
+
+class AttributeList(Resource):
+    def get(self):
+        try:
+            db = get_db()
+            collection = db.attributes.find({}, {'_id': False})
+            return [attr for attr in collection], 200
+        except Exception:
+            abort(500, description="Internal Server Error")
